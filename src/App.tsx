9bfe38550876cb4aba4fc04de837cbad0734dcfa
johannpereze <sebas.jsp@gmail.com>
2025-01-01 12:51:35 -0500

    feat(app): created due date to prevent too many fetches to endpoint

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,46 @@
 import { useEffect } from "react";
 import { useLocalStorage } from "usehooks-ts";
 
+const minutesBeforeExpiration = 1;
+
 function App() {
-  const [value, setValue] = useLocalStorage("apiData", "");
+  const [value, setValue] = useLocalStorage<any>("apiData", {});
+  console.log("🚀 ~ App ~ value:", value);
+
+  const fetchData = async () => {
+    try {
+      const response = await fetch(
+        "https://jsonplaceholder.typicode.com/posts/1"
+      );
+      const data = await response.json();
+
+      // Adds a predetermined amount of minutes to the current date
+      const dueDate = new Date();
+      dueDate.setMinutes(dueDate.getMinutes() + minutesBeforeExpiration);
+
+      data.dueTimestamp = dueDate.getTime();
+
+      setValue(data);
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
+
   useEffect(() => {
-    fetch("https://jsonplaceholder.typicode.com/posts/1")
-      .then((response) => response.json())
-      .then((data) => setValue(data))
-      .catch((error) => console.error("Error fetching data:", error));
-  }, []);
-  return <>{JSON.stringify(value, null, 2)}</>;
+    const storedData = value;
+
+    if (!storedData || !storedData.dueTimestamp) {
+      fetchData();
+    } else {
+      const dueDate = new Date(storedData.dueTimestamp);
+
+      if (dueDate < new Date()) {
+        fetchData();
+      }
+    }
+  }, [setValue]);
+
+  return <pre>{JSON.stringify(value, null, 2)}</pre>;
 }
 
 export default App;
