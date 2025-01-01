a577fc9b13b76adb8b46a2808c026b73e2881b29
johannpereze <sebas.jsp@gmail.com>
2025-01-01 12:35:19 -0500

    feat(app): created first fetch and installed local storage hook. also removed exhaustive dept lint rule

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,15 @@
+import { useEffect } from "react";
+import { useLocalStorage } from "usehooks-ts";
+
 function App() {
-  return <>Hello world</>;
+  const [value, setValue] = useLocalStorage("apiData", "");
+  useEffect(() => {
+    fetch("https://jsonplaceholder.typicode.com/posts/1")
+      .then((response) => response.json())
+      .then((data) => setValue(data))
+      .catch((error) => console.error("Error fetching data:", error));
+  }, []);
+  return <>{JSON.stringify(value, null, 2)}</>;
 }
 
 export default App;
