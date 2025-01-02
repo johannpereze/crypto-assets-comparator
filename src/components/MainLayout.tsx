28426046fecf80a49e6991ca4efccedcdea48250
johannpereze <sebas.jsp@gmail.com>
2025-01-01 19:32:09 -0500

    feat(app): added logo

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -1,4 +1,5 @@
 import { FC, PropsWithChildren } from "react";
+import { ReactComponent as Logo } from "../assets/logo.svg";
 import { ModeToggle } from "./ModeToggle";
 import { Separator } from "./ui/separator";
 
@@ -7,11 +8,9 @@ const MainLayout: FC<PropsWithChildren> = ({ children }) => {
     <>
       <header className="fixed  left-0 right-0 top-0 z-10 flex justify-center bg-background px-4">
         <div className="flex h-20 w-full max-w-2xl items-center justify-between ">
-          <div>
+          <div className={`flex items-center gap-2`}>
+            <Logo className="h-12 w-12" />
             <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-5xl">
-              <span className="text-xl font-extrabold tracking-tight lg:text-4xl">
-                🙃
-              </span>
               CA Comparator
             </h1>
           </div>
--- vite.config.ts
+++ vite.config.ts
@@ -1,10 +1,11 @@
+import svgr from "@svgr/rollup";
 import react from "@vitejs/plugin-react";
 import path from "path";
 import { defineConfig } from "vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [react()],
+  plugins: [react(), svgr()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "./src"),
