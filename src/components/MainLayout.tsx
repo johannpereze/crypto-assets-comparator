66ba19f9d9ec046a26040acae1522b51aa6fad86
johannpereze <sebas.jsp@gmail.com>
2025-01-02 17:41:41 -0500

    feat(app): modified logo

--- src/components/MainLayout.tsx
+++ src/components/MainLayout.tsx
@@ -6,11 +6,11 @@ import { Separator } from "./ui/separator";
 const MainLayout: FC<PropsWithChildren> = ({ children }) => {
   return (
     <>
-      <header className="fixed  left-0 right-0 top-0 z-10 flex justify-center bg-background px-4">
+      <header className="fixed left-0 right-0 top-0 z-10 flex justify-center bg-background px-4">
         <div className="flex h-20 w-full max-w-2xl items-center justify-between ">
-          <div className={`flex items-center gap-2`}>
-            <Logo className="h-12 w-12" />
-            <h1 className="scroll-m-20 text-2xl font-extrabold tracking-tight lg:text-5xl">
+          <div className={`flex items-center`}>
+            <Logo className="h-14 w-14" />
+            <h1 className="scroll-m-20 font-semibold tracking-tight lg:text-5xl">
               CA Comparator
             </h1>
           </div>
