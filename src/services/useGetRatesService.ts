52cdfb96c8fa9faacf4bc9eb8aa9ce14dd9076d0
johannpereze <sebas.jsp@gmail.com>
2025-01-02 21:19:40 -0500

    feat(app): added chart design with fake data

--- src/services/useGetRatesService.ts
+++ src/services/useGetRatesService.ts
@@ -57,6 +57,39 @@ export default function useGetRatesService() {
       console.error("Error fetching data:", error);
     }
   };
+  const fetchHistorical = async () => {
+    const headers = new Headers();
+    headers.append("x-api-key", import.meta.env.VITE_TATUM_API_KEY || "");
+    headers.append("accept", "application/json");
+    headers.append("content-type", "application/json");
+
+    // const body = testingAssets; // TODO: Replace with real data
+
+    try {
+      const response = await fetch(
+        `https://api.tatum.io/v3/tatum/marketdata/historical?symbol=BTC&interval=1day&start=1609459200&end=1640995200`,
+        {
+          method: "GET",
+          headers,
+          // body: JSON.stringify(body),
+        }
+      );
+
+      // if (response.status !== 201) {
+      //   throw new Error("Error fetching data");
+      // }
+      const data = await response.json();
+      console.log("🚀 ~ fetchHistorical ~ data:", data);
+
+      // Adds a predetermined amount of minutes to the current date
+      // const dueDate = new Date();
+      // dueDate.setMinutes(dueDate.getMinutes() + tatumFetchExpirationInMinutes);
+
+      // setValue({ rates: data, dueTimestamp: dueDate.getTime() });
+    } catch (error) {
+      console.error("Error fetching data:", error);
+    }
+  };
 
   useEffect(() => {
     const storedData = value;
@@ -70,6 +103,7 @@ export default function useGetRatesService() {
         fetchRates();
       }
     }
+    fetchHistorical();
   }, [setValue]);
 
   return { rates: value };
